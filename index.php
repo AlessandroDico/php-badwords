c2a1988e16c071eee7ed11840c88fb85d444f1d3
Ale <alessandrochb@gmail.com>
2020-12-14 15:16:36 +0100

    preso valore querySting e riportato in un paragrafo, sostituito con ***

--- index.php
+++ index.php
@@ -1,13 +1,22 @@
 <?php
-    
+    // inserire una queryString nel url ?badword='qualsiasiParola'
+
+    $censored = $_GET["badword"];
+
+    $paragraph = "la parola censurata è " . $censored . ", quindi non dovresti leggere la parola " . $censored . "!";
+
+    $finalParagraph = str_replace($censored, '***', $paragraph);
 ?>
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
     <head>
         <meta charset="utf-8">
-        <title>Php</title>
+        <title>Censored</title>
     </head>
     <body>
+
+        <p> <?php echo $finalParagraph ?> </p>
+
 
     </body>
 </html>
