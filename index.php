d694f2076948664c6693d02918730a241ec16db4
Ale <alessandrochb@gmail.com>
2020-12-14 15:50:29 +0100

    cambiata soluzione esercizio

--- index.php
+++ index.php
@@ -3,7 +3,8 @@
 
     $censored = $_GET["badword"];
 
-    $paragraph = "la parola censurata è " . $censored . ", quindi non dovresti leggere la parola " . $censored . "!";
+    // $paragraph = "la parola censurata è " . $censored . ", quindi non dovresti leggere la parola " . $censored . "!";
+    $paragraph = "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.";
 
     $finalParagraph = str_replace($censored, '***', $paragraph);
 ?>
@@ -18,7 +19,7 @@
         <p> <?php echo $finalParagraph ?> </p>
 
         <!-- la lunghezza del paragrafo originale è data dalla somma dei caratteri e degli spazi.. ovviamente cambia a seconda della parola inserita nella queryString -->
-        <p> il paragrafo originale è lungo <?php echo strlen($paragraph) ?> caratteri </p>
+        <p> il paragrafo originale è lungo <?php echo strlen($paragraph); ?> caratteri </p>
 
     </body>
 </html>
